66e194dbf1eb0db564524d6c47161128b061a240
paudelrajan007 <rajan11paudel@gmail.com>
2025-01-13 22:17:53 +0530

    learning python 13.01.25

---
+++ controlflow.py
@@ -0,0 +1,8 @@
+#Write a Python program to check if a number is positive, negative, or zero.
+num=int(input("num :"))
+if num > 0 :
+    print("number is positive")
+elif num == 0 :
+    print("number is zero")
+else :
+    print(" number is negative")
---
+++ controlflow01.py
@@ -0,0 +1,11 @@
+#Create a program to find the largest of three numbers.
+a=int(input("num 1 :"))
+b=int(input("num 2 : "))
+c=int(input("num 3 : "))
+if a>b & a>c :
+    print("num1 is the largest num among 3")
+elif b>a & b>c :
+     print("num2 is the largest num among 3")
+else :
+      print("num3 is the largest num among 3")
+    
