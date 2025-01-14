1613c045f24422ddd8e97e795759b5b4cb69efef
paudelrajan007 <rajan11paudel@gmail.com>
2025-01-14 20:02:29 +0530

    hi

--- practiceque05.py
+++ practiceque05.py
@@ -6,3 +6,6 @@ name=(input("Name:"))
 sirname=(input("Sirname:"))
 country=(input("Country:"))
 print("Name:",name+"\n"+"sirname:",sirname+"\n"+"country:",country)
+# to find length of str
+#function len(str)
+print(len(name))
