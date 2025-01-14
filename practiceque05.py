a6138cf78d933ce48b9edf669a9377ffe58fd5f5
paudelrajan007 <rajan11paudel@gmail.com>
2025-01-14 19:54:30 +0530

    14.01.25

---
+++ practiceque05.py
@@ -0,0 +1,8 @@
+"""print Name: 
+         Sirname: 
+         Country:    """
+
+name=(input("Name:"))
+sirname=(input("Sirname:"))
+country=(input("Country:"))
+print("Name:",name+"\n"+"sirname:",sirname+"\n"+"country:",country)
