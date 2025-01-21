363c9691c85d64686e8f5e86bbf2ff5028dcf8aa
paudelrajan007 <rajan11paudel@gmail.com>
2025-01-22 01:56:21 +0530

    22

---
+++ oddevenfunction.py
@@ -0,0 +1,9 @@
+# Write a Python function that takes an integer and checks whether it is odd or even.
+def book():
+ num=int(input("Enter a number :"))
+ if num %2 == 0 :
+    print("The number is even :",num)
+ else :
+   print("The number is odd :",num)
+book()
+book()
