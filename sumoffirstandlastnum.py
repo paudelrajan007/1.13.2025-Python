00ab6e70a5782e254a12caa521109ae034d51638
paudelrajan007 <rajan11paudel@gmail.com>
2025-01-22 11:46:59 +0530

    hi

--- sumoffirstandlastnum.py
+++ sumoffirstandlastnum.py
@@ -1,4 +1,4 @@
- # Write a python program to sum of first and last digits of 4 digits numbers.
+# Write a python program to sum of first and last digits of 4 digits numbers.
 def sum_of_first_and_last_digits():
     num=int(input("Enter four digits numbers :"))
     first_digit=num//1000
@@ -6,6 +6,4 @@ def sum_of_first_and_last_digits():
     sum_of_digits=first_digit+last_digit
     print(f"sum of first digit {first_digit} and last digit {last_digit} is : {sum_of_digits}")
     
-sum_of_first_and_last_digits() 
-sum_of_first_and_last_digits() 
-
+sum_of_first_and_last_digits()
