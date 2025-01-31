b97258b30da62cafcfc4f844ab165b4f1c5afbd6
paudelrajan007 <rajan11paudel@gmail.com>
2025-02-01 00:04:44 +0530

    pattern

--- __pycache__/pattern01.py
+++ __pycache__/pattern01.py
@@ -5,8 +5,8 @@
 #  *****
 pattern=0
 for _ in range(1,5+1):
-    print("*")
-    for j in range(1,6):
-        print("*")
-        print(j,end=" ")
-print()
+    
+    for j in range(_):
+        
+        print("*",end=" ")
+    print()
